aa71bfb55e8e2bd550e12a10e9fa318f0e262a1b
Furqaan Indori <indorif0227@gmail.com>
2023-12-22 18:48:12 -0600

    Formatted everything with prettier and removed unused variables

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import "normalize.css"
-import App from "./App";
-import { ThemeProvider, createTheme } from "@mui/material";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import 'normalize.css';
+import App from './App';
+import { ThemeProvider, createTheme } from '@mui/material';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 const theme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-  typography: {
-    fontFamily: [
-      "Poppins",
-      "Rubik",
-      "-apple-system",
-      "BlinkMacSystemFont",
-      "'Segoe UI'",
-      "Roboto",
-      "'Helvetica Neue'",
-      "Arial",
-      "sans-serif",
-    ].join(", "),
-  },
+	palette: {
+		mode: 'dark',
+	},
+	typography: {
+		fontFamily: [
+			'Poppins',
+			'Rubik',
+			'-apple-system',
+			'BlinkMacSystemFont',
+			"'Segoe UI'",
+			'Roboto',
+			"'Helvetica Neue'",
+			'Arial',
+			'sans-serif',
+		].join(', '),
+	},
 });
 
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>,
 );
