001ebedeaba1b8f92d2e1211e1928a7052b94ef6
llarse <clarsen908@gmail.com>
2024-01-22 18:22:39 -0600

    Invalid DNA edgecasing

--- src/components/util.js
+++ src/components/util.js
@@ -1,8 +1,15 @@
 export const addSpaceBeforeNumbers = (text) => {
+	if (typeof text !== 'string') {
+		return text;
+	}
 	return text.replace(/(\d+)/g, ' $1');
 };
 
 export const camelCaseToTitleCase = (text) => {
+	if (typeof text !== 'string') {
+		return text;
+	}
+
 	return text
 		.split(/(?=[A-Z])/)
 		.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
@@ -10,10 +17,19 @@ export const camelCaseToTitleCase = (text) => {
 };
 
 export const replaceDashesAndUnderscores = (text) => {
+	if (typeof text !== 'string') {
+		return text;
+	}
+
 	return text.replace(/[_-]/g, ' ');
 };
 
 export const snakeCaseToTitleCase = (text) => {
+	if (typeof text !== 'string') {
+		return text;
+	}
+
+
 	return text
 		.split('_')
 		.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
@@ -21,6 +37,11 @@ export const snakeCaseToTitleCase = (text) => {
 };
 
 export const toTitleCase = (text) => {
+	if (typeof text !== 'string') {
+		return text;
+	}
+
+
 	return text.replace(/\w\S*/g, function (match) {
 		return match.charAt(0).toUpperCase() + match.substr(1).toLowerCase();
 	});
