4b07dce06ce5f51dca87a74e7b961fd4e56c5534
taylorhoyt <31395207+taylorhoyt@users.noreply.github.com>
2024-04-30 14:24:51 -0500

    Added Persona image source

--- src/components/PersonaImage.jsx
+++ src/components/PersonaImage.jsx
@@ -4,7 +4,7 @@ function PersonaImage({ dna }) {
 
 	return (
 		<div>
-			<object data={`https://${process.env.REACT_APP_PERSONA_URL}${dna}`} type="image/png">
+			<object data={`https://5x1vj0debk.execute-api.us-east-1.amazonaws.com/default/fetch_persona_photo_from_dna?dna=0x${dna}`} type="image/png">
 				<p>Couldn't find persona photo</p>
 			</object>
 		</div>
